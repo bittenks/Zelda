bdb2255c08d26f5d07fcba7c981402129fee6f45
Guilherme Bittencourt <guilhermebittencourtcorrea@gmail.com>
2023-08-10 10:26:06 -0300

    End Games Exposition

--- src/Components/Games/Games.jsx
+++ src/Components/Games/Games.jsx
@@ -2,24 +2,34 @@ import React, { Component } from 'react'
 import './style.css'
 import OnlyGame from './OnlyGame';
 import Banner from '../../assets/Imgs/Banner.png'
+import botw from '../../assets/Imgs/botw.png'
+import Expansion from '../../assets/Imgs/Expansion.png'
+import HyruleWarriors from '../../assets/Imgs/HyruleWarriors.png'
+import LinkAwaking from '../../assets/Imgs/LinkAwaking.png'
+import Skyward from '../../assets/Imgs/Skyward.png'
+
+
+
+
+
 export class Games extends Component {
   render() {
     return (
         <>
       <div className='flex justify-center mt-2'>
-        <h3 className=''>Zelda para Nintendo Switch</h3>
+        <h3 className='GamesHeader'>Zelda para Nintendo Switch</h3>
       </div>
       <div className='GradeGames'>
-        <OnlyGame name={"The Legend of Zelda: Tears of the Kingdom"} Price={'357,99'} Gold={"299"} Img={Banner} />
-        <OnlyGame name={"The Legend of Zelda: Tears of the Kingdom"} Price={'357,99'} Gold={"299"} Img={Banner} />
-        <OnlyGame name={"The Legend of Zelda: Tears of the Kingdom"} Price={'357,99'} Gold={"299"} Img={Banner} />
+        <OnlyGame name={"The legend of Zelda : Tears of the Kingdom"} Price={'357,99'} Gold={"299"} Img={Banner} />
+        <OnlyGame name={"The legend of Zelda : Breath of the Wild"} Price={'357,99'} Gold={"299"} Img={botw} />
+        <OnlyGame name={"The legend of Zelda : BOTW Expansion Pass"} Price={'357,99'} Gold={"299"} Img={Expansion} />
         </div>
 
         <div className='GradeGames'>
 
-        <OnlyGame name={"The Legend of Zelda: Tears of the Kingdom"} Price={'357,99'} Gold={"299"} Img={Banner} />
-        <OnlyGame name={"The Legend of Zelda: Tears of the Kingdom"} Price={'357,99'} Gold={"299"} Img={Banner} />
-        <OnlyGame name={"The Legend of Zelda: Tears of the Kingdom"} Price={'357,99'} Gold={"299"} Img={Banner} />
+        <OnlyGame name={"The legend of Zelda : Tears of the Kingdom"} Price={'357,99'} Gold={"299"} Img={Skyward} />
+        <OnlyGame name={"The legend of Zelda : Link’s Awakening"} Price={'357,99'} Gold={"299"} Img={LinkAwaking} />
+        <OnlyGame name={"Hyrule Warrios: Age of Calamity"} Price={'357,99'} Gold={"299"} Img={HyruleWarriors} />
         </div>
         </>
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,52 +5,66 @@ export class Navbar extends Component {
     render() {
         return (
             <div className='flex flex-col'>
-            <div className='flex justify-between mt-1 Navbar'>
-                <div className='NintendoLogo'>
-                    <img src={NintendoLogo} alt="" srcset=""/>
-                </div>
-                <div className='flex items-center InputSearch '>
-                    <input className='InputSearch' type="text" placeholder='Pesquise o jogo'/>
-                </div>
-                <div className='flex items-center InfosNavbar '>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="25"
-                        height="24"
-                        viewBox="0 0 25 24"
-                        fill="none">
-                        <path
-                            d="M4.32217 6.31802C2.56481 8.07538 2.56481 10.9246 4.32217 12.682L12.0042 20.364L19.6861 12.682C21.4435 10.9246 21.4435 8.07538 19.6861 6.31802C17.9288 4.56066 15.0795 4.56066 13.3222 6.31802L12.0042 7.63609L10.6861 6.31802C8.92877 4.56066 6.07953 4.56066 4.32217 6.31802Z"
-                            stroke="#3A3A3C"
-                            stroke-width="1.5"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"/>
-                    </svg>
-                    <span>Adicionar a lista de Desejo</span>
-                </div>
-                <div className='flex items-center InfosNavbar '>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="25"
-                        height="24"
-                        viewBox="0 0 25 24"
-                        fill="none">
-                        <path
-                            d="M5.12519 17.8037C7.15682 16.6554 9.50395 16 12.0042 16C14.5044 16 16.8515 16.6554 18.8831 17.8037M15.0042 10C15.0042 11.6569 13.661 13 12.0042 13C10.3473 13 9.00415 11.6569 9.00415 10C9.00415 8.34315 10.3473 7 12.0042 7C13.661 7 15.0042 8.34315 15.0042 10ZM21.0042 12C21.0042 16.9706 16.9747 21 12.0042 21C7.03359 21 3.00415 16.9706 3.00415 12C3.00415 7.02944 7.03359 3 12.0042 3C16.9747 3 21.0042 7.02944 21.0042 12Z"
-                            stroke="#3A3A3C"
-                            stroke-width="1.5"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"/>
-                    </svg>
-                    <span>Acessar minha conta</span>
-                </div>
-                
-            </div>
-            <div className='line'>
+                <div className='flex justify-between  Navbar'>
+                    <div className='NintendoLogo'>
+                        <img src={NintendoLogo} alt="" srcset=""/>
+                    </div>
+                    <div className='flex items-center InputSearch'>
 
-            </div>
-            </div>
+                        <input className='InputSearch' type="text" placeholder='Pesquise o jogo'/>
+                        <div className="search-icon">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="#A9A9A9"
+                                className="w-6 h-6">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
+                            </svg>
 
+                        </div>
+                    </div>
+
+                    <div className='flex items-center InfosNavbar '>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="25"
+                            height="24"
+                            viewBox="0 0 25 24"
+                            fill="none">
+                            <path
+                                d="M4.32217 6.31802C2.56481 8.07538 2.56481 10.9246 4.32217 12.682L12.0042 20.364L19.6861 12.682C21.4435 10.9246 21.4435 8.07538 19.6861 6.31802C17.9288 4.56066 15.0795 4.56066 13.3222 6.31802L12.0042 7.63609L10.6861 6.31802C8.92877 4.56066 6.07953 4.56066 4.32217 6.31802Z"
+                                stroke="#3A3A3C"
+                                stroke-width="1.5"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"/>
+                        </svg>
+                        <span>Adicionar a lista de Desejo</span>
+                    </div>
+                    <div className='flex items-center InfosNavbar mr-5'>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="25"
+                            height="24"
+                            viewBox="0 0 25 24"
+                            fill="none">
+                            <path
+                                d="M5.12519 17.8037C7.15682 16.6554 9.50395 16 12.0042 16C14.5044 16 16.8515 16.6554 18.8831 17.8037M15.0042 10C15.0042 11.6569 13.661 13 12.0042 13C10.3473 13 9.00415 11.6569 9.00415 10C9.00415 8.34315 10.3473 7 12.0042 7C13.661 7 15.0042 8.34315 15.0042 10ZM21.0042 12C21.0042 16.9706 16.9747 21 12.0042 21C7.03359 21 3.00415 16.9706 3.00415 12C3.00415 7.02944 7.03359 3 12.0042 3C16.9747 3 21.0042 7.02944 21.0042 12Z"
+                                stroke="#3A3A3C"
+                                stroke-width="1.5"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"/>
+                        </svg>
+                        <span>Acessar minha conta</span>
+                    </div>
+
+                </div>
+                <div className='line'></div>
+            </div>
 
         )
     }
